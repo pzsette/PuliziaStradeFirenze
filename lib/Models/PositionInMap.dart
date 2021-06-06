977eb13fd40b3f9a893a31f2aeee3f9122b744f3
Pietro Zarri <pietrozarri96@gmail.com>
2021-06-06 09:40:01 +0200

    add position model

--- lib/Models/PositionInMap.dart
+++ lib/Models/PositionInMap.dart
@@ -1,1 +1,21 @@
+class PositionInMap {
+  String streetName;
+  String section;
+  String city;
 
+  PositionInMap(this.streetName, this.city, {this.section});
+
+  void addSection(String section) {
+    this.section = section;
+  }
+
+  @override
+  String toString() {
+    return ("streetName: " +
+        streetName +
+        ", section: " +
+        section +
+        ", city: " +
+        city);
+  }
+}
