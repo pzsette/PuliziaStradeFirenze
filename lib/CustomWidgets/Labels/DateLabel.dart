5ac8d5ab5d117d6a698cad3fba80759a61216d42
Pietro Zarri <pietrozarri96@gmail.com>
2021-06-06 10:13:25 +0200

    add date and position label

---
+++ lib/CustomWidgets/Labels/DateLabel.dart
@@ -0,0 +1,37 @@
+import 'package:firenze_pulizia_strade/Utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:pulizia_strade/Models/PositionInMap.dart';
+
+class DateLabel extends StatelessWidget {
+  final PositionInMap position;
+  final Map result;
+
+  DateLabel(this.position, this.result);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+        child: Column(
+      children: [
+        if (position.section != null &&
+            result["data"] != "indirizzo non trovato")
+          Text("Prossima pulizia:",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: screenWidth / 30)),
+        (position.section != null &&
+                (result["data"] != "indirizzo non trovato"))
+            ? Text(convertDate(result['data']) + " alle " + result['ora'],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: screenWidth / 22, fontWeight: FontWeight.w500))
+            : Text(
+                "Strada non presente nel database di pulizia strade",
+                style: TextStyle(
+                    fontSize: screenWidth / 22, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              )
+      ],
+    ));
+  }
+}
